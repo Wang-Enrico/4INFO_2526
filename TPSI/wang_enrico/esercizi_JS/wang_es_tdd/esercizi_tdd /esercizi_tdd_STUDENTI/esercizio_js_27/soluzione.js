0ce4b0946512314d56ffa11f1567f35434484eb6
Wang-Enrico <enricowang784@gmail.com>
2026-01-29 13:24:52 +0100

    esercizi_tdd_STUDENTI/

--- TPSI/wang_enrico/esercizi_JS/wang_es_tdd/esercizi_tdd /esercizi_tdd_STUDENTI/esercizio_js_27/soluzione.js
+++ TPSI/wang_enrico/esercizi_JS/wang_es_tdd/esercizi_tdd /esercizi_tdd_STUDENTI/esercizio_js_27/soluzione.js
@@ -1,4 +1,6 @@
 export const capitalize_all = (str) => {
    //INSERISCI QUI IL TUO CODICE
+   return str.split(' ').map(word => {
+         return word.charAt(0).toUpperCase() + word.slice(1);
+   }).join(' ');  
 }
-
